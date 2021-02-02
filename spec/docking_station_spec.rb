e5c8e9e296dc4bb02df5e68fd53194778940dcc2
Olga <itsglimerry@gmail.com>
2021-02-02 17:18:21 +0000

    Updated tests and methods for docking station to release working bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,20 @@
 require 'docking_station'
+require 'bike.rb'
 
+  
 describe DockingStation do
 
-  it { is_expected.to respond_to :release_bike }
+    
+subject(:docking_station) {described_class.new}
+  
+  it { is_expected.to respond_to :release_bike}
+
+
+it 'releases the bike' do
+expect(docking_station.release_bike.working?).to eq true
+
 
 end
+
+
+end 
