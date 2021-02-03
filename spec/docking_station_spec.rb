c0ee1a9d97f1b65d62a9e81522707d050601d5e4
Joey <joey.s.hornsby@gmail.com>
2021-02-03 15:58:04 +0000

    added passing tests for dock_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,19 @@
 require 'docking_station'
 
 describe DockingStation do
-subject(:docking_station) {described_class.new}
+subject(:docking_station) { described_class.new }
+let(:bike) { Bike.new }
 
   it { is_expected.to respond_to :release_bike}
   it { is_expected.to respond_to :dock_bike}
 
   it "bike working?" do
     expect(docking_station.release_bike).to be_instance_of(Bike)
-    
-    bike = docking_station.release_bike
     expect(bike).to be_working
   end
-  
+
+  it "dock bike" do
+    docking_station.dock_bike(bike)
+    expect(docking_station.has_bike).to eq true
+  end
 end
