6e6e66185dc051e7cc14c1bf486b79485848edee
Joey <joey.s.hornsby@gmail.com>
2021-02-03 14:18:19 +0000

    went back to end of challenge 8 to pair with Jack

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,15 +1,9 @@
 require 'docking_station'
-require 'bike'
 
 
 describe DockingStation do
 subject(:docking_station) {described_class.new}
 
   it { is_expected.to respond_to :release_bike}
-
-  it 'releases the bike' do
-    expect(docking_station.release_bike).to be_instance_of(Bike)
-    expect(docking_station.release_bike).to be_working
-  end
 
 end
