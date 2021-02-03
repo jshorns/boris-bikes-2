2f46c9ef0b1d40aa684943b1f37623b05f2e28b2
Jack Gumoes <j.gumoes@gmail.com>
2021-02-03 17:37:11 +0000

    adjusted script.rb to import bike.rb and docking_station.rb, regardless of the working directory

--- lib/script.rb
+++ lib/script.rb
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 path = File.expand_path(File.dirname(__FILE__)).to_s
 
 require "#{path}/bike.rb"
 require "#{path}/docking_station.rb"
-=======
-require "./bike.rb"
-require "./docking_station.rb"
->>>>>>> afa177c1c524081eb63b063cae63127490f77193
