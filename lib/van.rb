bcb6179859efe5fdc89352cfab2365f3eb6a3fec
Joey <joey.s.hornsby@gmail.com>
2021-02-08 21:45:34 +0000

    new methods for taking and picking up bikes in van class - committing now as I want to try a different method

--- lib/van.rb
+++ lib/van.rb
@@ -1,15 +1,42 @@
 class Van
-  attr_reader :bikes_on_van
+  attr_reader :bikes
 
   def initialize
-    @bikes_on_van = []
+    @bikes = []
   end
 
-  def pick_up_broken(docking_station)
-    @bikes_on_van = docking_station.bikes.select {|bike| !bike.working? }
-    docking_station.bikes.reject! {|bike| !bike.working? }
+  def take_broken(obj)
+    obj.select {|bike| !bike.working? }
   end
 
-  def drop_off(garage)
+  def take_fixed(obj)
+    obj.select {|bike| bike.working? }
   end
+
+  def leave_broken(obj)
+    obj.select! {|bike| !bike.working? }
+  end
+
+  def leave_fixed(obj)
+    obj.select! {|bike| bike.working? }
+  end
+
+  def pick_up_for_repair(docking_station)
+    @bikes << take_broken(docking_station.bikes)
+    @bikes.flatten!
+    leave_fixed(docking_station.bikes)
+  end
+
+  def drop_off_for_repair(garage)
+    garage.bikes << take_broken(@bikes)
+    garage.bikes.flatten!
+    leave_fixed(@bikes)
+  end
+
+  def pick_up_for_return(garage)
+    @bikes << take_fixed(garage.bikes)
+    @bikes.flatten!
+    leave_broken(garage.bikes)
+  end
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -6,17 +6,31 @@ require 'van'
 describe Van do
 subject(:van) { described_class.new }
   let(:docking_station) { double(:docking_station, :bikes => [bike, broken_bike, bike, broken_bike, bike]) }
-  let(:garage) { double :garage }
+  let(:garage) { double(:garage, :bikes => [bike, bike, bike]) }
   let(:bike) { double(:bike, :working? => true) }
   let(:broken_bike) { double(:broken_bike, :working? => false) }
 
-  it { is_expected.to respond_to(:pick_up_broken).with(1).argument }
-  it { is_expected.to respond_to(:drop_off).with(1).argument }
+  it { is_expected.to respond_to(:pick_up_for_repair).with(1).argument }
+  it { is_expected.to respond_to(:drop_off_for_repair).with(1).argument }
 
   it 'picks up broken bikes from docking station' do
-    van.pick_up_broken(docking_station)
-    expect(van.bikes_on_van).to eq [broken_bike, broken_bike]
+    van.pick_up_for_repair(docking_station)
+    expect(van.bikes).to eq [broken_bike, broken_bike]
     expect(docking_station.bikes).to eq [bike, bike, bike]
+  end
+
+  it 'drops off broken bikes at garage' do
+    van.pick_up_for_repair(docking_station)
+    van.drop_off_for_repair(garage)
+    expect(van.bikes).to eq []
+    expect(garage.bikes).to eq [bike, bike, bike, broken_bike, broken_bike]
+  end
+
+  it 'picks up fixed bikes from garage' do
+    van.pick_up_for_return(garage)
+    puts garage.bikes
+    expect(van.bikes).to eq [bike, bike, bike]
+    expect(garage.bikes).to eq []
   end
 
 end
